470937e4d2691cb33947ec55c03f607536acf495
Michael Davidsaver <mdavidsaver@bnl.gov>
2012-01-15 13:37:10 -0500

    combine getpvs methods

---
+++ pyPDB/getdbd.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+from warnings import warn
+from collections import defaultdict
+    
+from pyPDB.po import POEnt
+from pyPDB.dbd.expand import loadDBD, DBD
+
+linktypes = ['DBF_INLINK', 'DBF_OUTLINK', 'DBF_FWDLINK']
+infofields = ['EGU', 'DESC']
+
+def main(opts,args,out):
+    
+    pdb=[]
+    
+    pcache=defaultdict(dict)
+    
+    for inp in args:
+        pdb+=loadDBD(inp, path=opts.include, cache=pcache)
+    
+    pdb=DBD(pdb)
+    
+    pdb.singular('recordtype')
+    if not opts.dups:
+        pdb.singular('record')
+    
+    def cleanLink(val):
+        val=val.strip()
+        v,_,_=val.partition('.')
+        v,_,_=v.partition(' ')
+        return v.strip()
+    
+    # list of translatable fields by recordtype
+    recflds={}
+    
+    entries={}
+    
+    def allentries(sec):
+        for s in sec:
+            assert isinstance(s, list)
+            for inst in s:
+                yield inst
+    
+    if opts.dups:
+        I=allentries(pdb.records.itervalues())
+    else:
+        I=pdb.records.itervalues()
+    
+    for rec in I:
+        #print rec,flds[0],
+    
+        try:
+            ent = entries[rec.name]
+        except KeyError:
+            ent = POEnt(rec.name)
+            entries[rec.name] = ent
+    
+        # prepend definition(s)
+        ent.comExt.insert(0, 'recordtype: %s'%rec.rec)
+        ent.refs.insert(0, '%s:%d'%(rec.name.file, rec.name.lineno))
+    
+        rtype=pdb.recordtypes.get(rec.rec, None)
+        if rtype is None:
+            warn("%s : Skipping unknown recordtype '%s'"%(rec.name,rec.rec))
+            continue
+    
+        interesting=recflds.get(rtype,  None)
+        if interesting is None:
+            interesting=[]
+            for rf in rtype.fields:
+                if rf.dbf in linktypes:
+                    interesting.append(rf.name)
+            recflds[rtype]=interesting
+    
+        for fld in rec.fields:
+            if fld.name in interesting:
+                #print fld.name,
+                try:
+                    float(fld.value)
+                    # Constant link
+                    continue
+                except ValueError:
+                    pass
+                
+                val=cleanLink(fld.value)
+    
+                if len(val)==0 or val[0]=='@':
+                    continue
+        
+                try:
+                    valent=entries[val]
+                except KeyError:
+                    valent=POEnt(val)
+                    entries[val] = valent
+                
+                valent.refs.append('%s:%d'%(fld.value.file, fld.value.lineno))
+     
+            if fld.name in infofields:
+                ent.comExt.append('%s: %s'% \
+                    (fld.name, repr(fld.value)[1:-1]))
+    
+        #print
+    
+    return entries
---
+++ pyPDB/getedl.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+import re, sys
+from warnings import warn
+
+from pyPDB.dbd.expand import findFile
+from pyPDB.po import POEnt
+
+
+def main(opts,args,out):
+    
+    attrpat=re.compile('^\s* (\S+) \s* "(\\"|[^"]*)" \s*$', re.X)
+    
+    # widget attributes known to contain PV names
+    PVattrs=[
+    'alarmPv',
+    'colorPv',
+    'controlPv',
+    'filePv',
+    'indicatorPv',
+    'readPv',
+    'visPv',
+    'xPv',
+    'yPv',
+    ]
+    
+    # Just a record name with optional field
+    #   record.FLD
+    plainpv=re.compile('^\s*([^.]+)\.?.*$')
+    
+    
+    def extractRec(s):
+        """ Extract record name(s) from string
+        """
+        
+        if s.startswith('CALC'):
+            # May contain several record names
+            #   CALC\\\{(((A=0)&&(B=0)))\}($(P)$(M).DMOV, $(P)$(M).STAT)
+    
+            _,_,pvs = s.partition('\\}')
+            pvs=pvs.strip().strip('()')
+    
+            if len(pvs)==0:
+                warn('CALC with no PVs? "%s"'%s)
+                return []
+            
+            pvs=pvs.split(',')
+            pvs=map(str.strip, pvs)
+            # strip field names
+            pvs=map(lambda p:extractRec(p)[0],pvs)
+    
+            return pvs
+    
+        else:
+            M=plainpv.match(s)
+            if M is None:
+                return []
+            return [M.group(1)]
+    
+    entries={}
+    
+    for fname in args:
+    
+        try:
+            fullname=findFile(fname, path=opts.include, env='EDMDATAFILES') 
+    
+            fd=open(fullname, 'rU')
+    
+        except IOError:
+            print >>sys.stderr, "Can't find/read",fname,"in",', '.join(opts.include)
+            sys.exit(1)
+    
+        for ln, L in enumerate(fd.readlines()):
+            #print ln,L
+            M=attrpat.match(L)
+            if M is None:
+                continue
+    
+            if M.group(1) not in PVattrs:
+                continue
+            
+            pvs=extractRec(M.group(2))
+            pvs=list(set(pvs)) # make unique
+    
+            for pv in pvs:
+                try:
+                    ent=entries[pv]
+                except KeyError:
+                    ent=POEnt(pv)
+                    entries[pv]=ent
+    
+                if M.group(1) not in ent.comExt:
+                    ent.comExt.append(M.group(1))
+    
+                ent.refs.append('%s:%d'%(fname,ln))
+    
+            print fname,ln,pvs
+    
+        fd.close()
+    
+    return entries

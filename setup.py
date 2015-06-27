bb6c40cc090a981d206cb1a975521f06743e1efb
Michael Davidsaver <mdavidsaver@gmail.com>
2015-06-27 15:10:12 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ import sys
 
 from distutils.core import setup
 
-setup(name='pyPDB', 
-          version='0.1', 
-          description="Utilities for working with EPICS PDB files", 
-          author='Michael Davidsaver', 
-          author_email='mdavidsaver@bnl.gov', 
+setup(name='pyPDB',
+          version='0.1',
+          description="Utilities for working with EPICS PDB files",
+          author='Michael Davidsaver',
+          author_email='mdavidsaver@gmail.com',
           packages=['pyPDB',
                     'pyPDB.dbd',
                     'pyPDB.po',
-                   ], 
-          scripts=['getpvs', 'applypvs'], 
+                   ],
+          scripts=['getpvs', 'applypvs', 'dbdlint'],
           requires=
             ['ply (>=3.4)'],
       )
